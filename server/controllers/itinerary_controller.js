8c949f71e9eb8a476ff899a01b15c76215c5c4c4
gitbrendanlam <141107696+gitbrendanlam@users.noreply.github.com>
2024-04-13 14:37:42 -0700

    OpenAI API chat completion working

--- server/controllers/itinerary_controller.js
+++ server/controllers/itinerary_controller.js
@@ -1,1 +1,63 @@
 //Controller to call the Open AI API for information on destinations for the itinerary
+// import { Configuration, OpenAI } from "openai";
+const OpenAI = require('openai');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env'});
+const express = require('express');
+const app = express();
+
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI_API_KEY });
+
+// TEST DATA - DELETE WHEN FINISHED
+const travelPlans = {
+  destination: 'Los Angeles, CA',
+  startDate: 'June 2, 2024',
+  endDate: 'June 8, 2024',
+  activities: [],
+  budget: 500,
+  travelers: 1,
+  groupDescription: 'Solo traveler',
+  loading: false,
+  error: null,
+}
+// ========= end of TEST DATA ============
+
+const tripController = {
+  async buildTrip(req, res, next) {
+    // Update prompt below to reflect req.body information
+    const prompt = `Make an itinerary for a trip for ${travelPlans.travelers} to ${travelPlans.destination} from ${travelPlans.startDate} until ${travelPlans.endDate}. I have a budget of $${travelPlans.budget}. Include the following types of attractions: ${travelPlans.activities.join(', ')}. Organize the itinerary by the following times of day: morning, afternoon, and evening. Recommend specific places of interest with their address. Limit cross-city commutes by grouping places of interest by geography for each day. Output the response in json format following this schema:
+    {
+      itinerary: {
+        date: {
+          time of day: {
+            activity: string,
+            description: string,
+            address: string,
+          }
+        }
+      }
+    }
+    Thank you.`;
+
+    console.log(prompt);
+    try {
+      const completion = await openai.chat.completions.create({
+        messages: [{"role": "system", "content": "You are a helpful travel planning assistant."},
+            {
+              "role": "user", 
+              "content": prompt,
+            }],
+        model: "gpt-3.5-turbo",
+        response_format: { type: "json_object" },
+      });
+    
+      console.log(JSON.parse(completion.choices[0].message.content));
+      res.locals.itinerary = JSON.parse(completion.choices[0].message.content);
+      return next();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+module.exports = tripController;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require ('express');
 const path = require ('path');
 
+// TEST CODE - CAN DELETE WHEN FINISHED
+const tripController = require('./controllers/itinerary_controller');
+
 const app = express();
 const port = 3000;
 
@@ -10,6 +13,10 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'../index.html'))
+})
+
+app.get('/trip', tripController.buildTrip, (req, res) => {
+  res.status(200).send(res.locals.itinerary);
 })
 
 

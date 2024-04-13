090c7bd7f1880fb6991ab7473d10c50977f0316c
gitbrendanlam <141107696+gitbrendanlam@users.noreply.github.com>
2024-04-13 11:16:24 -0700

    some changes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const express = require ('express')
-const path = require ('path')
+const express = require ('express');
+const path = require ('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client')));

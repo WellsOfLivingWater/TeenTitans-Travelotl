03e744bc4f5d8aabd6ce40f172015f545c033eee
Dylan Carothers <dcarothers16@gmail.com>
2024-04-16 20:34:41 -0400

    Updated routes

---
+++ server/routes/form_route.js
@@ -0,0 +1,8 @@
+// const express = require('express');
+// const itineraryController = require('../controllers/itinerary_controller');
+// const { default: Form } = require('../../client/components/Form');
+// const router = express.Router();
+
+// router.get('/form', (req, res) =>{
+//     res.send(Form)
+// })

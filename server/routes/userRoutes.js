1ed2677709b033366dfa5eea86c150198de8708d
Jay Hoogheem <jayh1186@gmail.com>
2024-04-16 23:35:09 -0500

    refactored user routes & controller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, getUser } = require('../controllers/userController');
-const { protect } = require('../controllers/auth_controller')
+const { protect } = require('../controllers/auth_controller');
 
-router.post('/', registerUser)
-router.post('/login', loginUser)
-router.get('/user', protect, getUser)
+router.post('/', registerUser);
+router.post('/login', loginUser);
+router.get('/user', protect, getUser);
 
 module.exports = router;

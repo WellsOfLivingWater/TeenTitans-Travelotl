29f99078623ad827f0c5df96ab0360a3f04fc5a6
Jay Hoogheem <jayh1186@gmail.com>
2024-04-16 21:18:35 -0500

    Co-authored-by: gitbrendanlam <gitbrendanlam@users.noreply.github.com>

---
+++ server/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+// define userSchema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+},
+{
+  timestamps: true
+});
+
+// define user model
+const User = mongoose.model('User', userSchema);
+
+// export user model
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,23 @@
 const express = require ('express');
 const path = require ('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+
+//use environmental variables
+dotenv.config({ path: './config.env' });
+
+// connect to MongoDB cluster
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+connectDB();
 
 // TEST CODE - CAN DELETE WHEN FINISHED
 const tripController = require('./controllers/itinerary_controller');
@@ -9,7 +27,9 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.urlencoded({ extended: true })); //parse urlencoded bodies
 
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'../index.html'))

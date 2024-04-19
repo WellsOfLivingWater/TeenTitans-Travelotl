57008a47c9ad98e52406571b37d9e28f957e30ef
gitbrendanlam <141107696+gitbrendanlam@users.noreply.github.com>
2024-04-18 19:11:59 -0700

    brendan changes 4/18/2024

--- client/components/Manager.jsx
+++ client/components/Manager.jsx
@@ -1,10 +1,62 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { updateItinerary } from "../reducers/itineraryReducer";
+import { Link } from 'react-router-dom';
 
 const Manager = () => {
   const [itineraries, setItineraries] = useState([]);
+  const dispatch = useDispatch();
 
+  // Retrieve all itineraries associated with the user and update state
   useEffect(() => {
-    const getItineraryList = async () => {
+    try {
+      const getItineraryList = async () => {
+        let itineraryList = await fetch('api/trip/retrieve', {
+          method: 'GET',
+            headers: {
+              'Authorization': `Bearer ${localStorage.getItem('userToken')}`,
+            },
+        });
+  
+        itineraryList = await itineraryList.json();
+  
+        console.log(itineraryList);
+        setItineraries(itineraryList);
+  
+      }
+      getItineraryList();   
+    } catch (error) {
+      console.error('Error with request:', error);
+    }
+    
+  }, []);
+
+  const deleteItinerary = async (e) => {
+    const tripId = e.target.parentNode.parentNode.id;
+    try {
+      let remainingTrips = await fetch('api/trip/delete', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('userToken')}`,
+        },
+        body: JSON.stringify({ tripId: tripId }),
+      });
+
+      remainingTrips = await remainingTrips.json();
+
+      setItineraries(remainingTrips);
+
+    } catch (err) {
+      console.error('Error with request:', error);
+    }
+    
+  }
+
+  const seeDetails = async () => {
+    const tripId = e.target.parentNode.parentNode.id;
+
+    try {
       let itineraryList = await fetch('api/trip/retrieve', {
         method: 'GET',
           headers: {
@@ -14,23 +66,33 @@ const Manager = () => {
 
       itineraryList = await itineraryList.json();
 
-      // console.log(await itineraryList.json());
-      setItineraries(itineraryList);
+      let foundTrip;
+      for (const trip of itineraryList) {
+        if (trip.tripId === tripId) {
+          foundTrip = trip.trip;
+          break;
+        }
+      }
 
+      if (foundTrip) dispatch(updateItinerary(foundTrip));
+      // Route to adam's itinerary page HEREEEEE
+      
+    } catch (error) {
+      console.error('Error with request:', error);
     }
-    
-    getItineraryList();
-        
-  }, []);
+  };
 
   const itineraryList = [...itineraries];
   const renderList = itineraryList.map((itinerary) => {
-    return (<div>
+    return (<div key={itinerary._id} id={itinerary._id}>
       <h4>{itinerary.destination}</h4>
-      <p>From: <b>itinerary.startDate</b></p>
-      <p>To: <b>itinerary.endDate</b></p>
+      <p>From: <b>{itinerary.startDate}</b></p>
+      <p>To: <b>{itinerary.endDate}</b></p>
+      <div><button onClick={ seeDetails }>See Details</button>   <button onClick={ deleteItinerary }>Delete</button></div>
     </div>)
   })
+  // state: { itinerary: { itinerary: itinerary.trip }}
+  // to={{ pathname: '/other', state: dataToPass }}
   
 
   return (<div>

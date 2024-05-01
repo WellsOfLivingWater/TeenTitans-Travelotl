7d8b6d7dfdf186db90ec58a95e567c595706e2d6
WellsOfLivingWater <{107329536}+{WellsOfLivingWater}@users.noreply.github.com>
2024-04-30 21:44:55 -0400

    Made a temporary bootstrap navbar.

--- client/components/headerPages/Header.jsx
+++ client/components/headerPages/Header.jsx
@@ -6,27 +6,26 @@
  * @returns {JSX.Element} The rendered header component.
  */
 import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
 
   return (
-    <div className="header-container">
-      <div>
-        <Link to='/' className='text-blue-600 text-3xl font-bold font-serif text-center'>Travelotl</Link>
-      </div>
-      <div className='text-right m-2'>
-        <Link to='/manager'>Manager</Link>
-      </div>
-      <div className='text-right m-2'>
-        <Link to='/about'>About</Link>
-      </div>
-      <div className='text-center m-2'>
-        <Link to='/register'>Register</Link>
-      </div>
-      <div className='text-center m-2'>
-        <Link to='/login'>Login</Link>
-      </div>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={Link} to="/" className="text-blue-600 text-3xl font-bold font-serif text-center">Travelotl</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/manager">Manager</Nav.Link>
+          <Nav.Link as={Link} to="/friends">Friends</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          <Nav.Link as={Link} to="/register">Register</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 

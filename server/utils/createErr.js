14be496e46a0aa65b8292ee5e3a1cf02696f9314
WellsOfLivingWater <{107329536}+{WellsOfLivingWater}@users.noreply.github.com>
2024-05-03 01:04:35 -0400

    Enhanced createErr log.

--- server/utils/createErr.js
+++ server/utils/createErr.js
@@ -1,6 +1,6 @@
 const createErr = ({ method, type, err }) => {
   return {
-    log: `${method}: ${type}: ${err}`,
+    log: `${method}: ${type} ${err}`,
     message: { err: `Error occurred in ${method}. Check server logs for details.` },
   };
 };

96cd2ce70ddc69e94ee513a937b733068ec8e48f
WellsOfLivingWater <{107329536}+{WellsOfLivingWater}@users.noreply.github.com>
2024-05-03 01:10:31 -0400

    Formatted Signin.jsx for later debugging.

--- client/components/Signin.jsx
+++ client/components/Signin.jsx
@@ -19,44 +19,45 @@ export default function Signin(props) {
   return (
     <div className='modal'>
       <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      
-    >
-      <Modal.Header closeButton className='modal-header'>
-        <Modal.Title id="contained-modal-title-vcenter">
-        Sign in to unlock the best of Travelotl.
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className='container-login-modal'>
-        <div className='login-container'>
-        <button className='img-login-text'>
-            <div className='img-email'>
-              <img src={google} alt="google-logo" style={{width:'35px'}} />
-            </div>
-            <div className='text-email'>
-              <Link to="http://localhost:3000/api/auth/google" className="auth-link google">Continue with Google</Link>
-            </div>
-          </button>
-        </div>
-        <div className='login-container'>
-          <button className='img-login-text' onClick={() => setOpenLogin(true)}>
-            <div className='img-email'>
-              <img src={email} alt="email" style={{width:'30px', marginLeft:'2px'}} />
-            </div>
-            <div className='text-email'>
-              <Link to='/'>Continue with email</Link>
-            </div>
-          </button>
-          <Login
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton className='modal-header'>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Sign in to unlock the best of Travelotl.
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className='container-login-modal'>
+          <div className='login-container'>
+            <button className='img-login-text'>
+              <div className='img-email'>
+                <img src={google} alt="google-logo" style={{width:'35px'}} />
+              </div>
+              <div className='text-email'>
+                <Link to="http://localhost:3000/api/auth/google" className="auth-link google">
+                  Continue with Google
+                </Link>
+              </div>
+            </button>
+          </div>
+          <div className='login-container'>
+            <button className='img-login-text' onClick={() => setOpenLogin(true)}>
+              <div className='img-email'>
+                <img src={email} alt="email" style={{width:'30px', marginLeft:'2px'}} />
+              </div>
+              <div className='text-email'>
+                <Link to='/'>Continue with email</Link>
+              </div>
+            </button>
+            <Login
               show={openLogin}
               onHide={() => setOpenLogin(false)}
             />
-        </div>
-      </Modal.Body>
-    </Modal>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
